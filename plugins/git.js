67c8107b2cf8994a37f85a600cece0d53031b041
monster lallu <87799191+Monsterlallu@users.noreply.github.com>
2021-11-13 17:10:16 +0700

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -18,22 +18,22 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.GL}, (async (message
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {quoted: message.data , mimetype: Mimetype.png, caption: `*Ajuser_v2*
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {quoted: message.data , mimetype: Mimetype.png, caption: `*Lallu-sir*
  
 
-        *Github link        _https://github.com/aju001/Ajuser_v2_*
+        *Github link        _https://github.com/Monsterlallu/Lallu-sir_*
         
         
        *Bot making video   _https://youtu.be/dm_kVZ0m2eY_*
  
 
-       *Audio commads-1     _https://github.com/aju001/Ajuser_v2/tree/master/upload_*
+       *Audio commads-1     _https://github.com/Monsterlallu/Lallu-sir/tree/master/upload_*
  
  
-       *Audio commads-2     _https://github.com/aju001/Ajuser_v2/tree/master/uploads_*
+       *Audio commads-2     _https://github.com/Monsterlallu/Lallu-sir/tree/master/uploads_*
   
   
-      *Sticker commads      _https://github.com/aju001/Ajuser_v2/tree/master/stickers_*
+      *Sticker commads      _https://github.com/Monsterlallu/Lallu-sir/tree/master/stickers_*
 `}) 
 
 })); 
