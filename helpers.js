daa231bc0d225c25beff8d804b22f8aa3c1e6ba2
monster lallu <87799191+Monsterlallu@users.noreply.github.com>
2021-11-14 13:00:16 +0530

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *Ajuser*:  ```" + msg + "```"
+    return "✅ *Lallusir*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *Ajuser*:  ```" + msg + "```"
+    return "🛑 *Lallusir*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *Ajuser*:  ```" + msg + "```"
+    return "⏺️ *Lallusir*:  ```" + msg + "```"
 }
 
 
